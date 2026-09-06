f6d0853ea3bd4a9fce273398ac4d6729c5d1d6c1
Violl0 <maxim99911@bk.ru>
2026-09-06 13:46:01 +0300

    Step 7: variables and data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет от ИСП-241");
-Console.WriteLine("Автор: Каторжин Максим");
-Console.WriteLine("Гот: 2026");
+﻿string firstName = "Максим";
+string lastName = "Каторжин";
+string group = "ИСП-241";
+int birthYear = 2008;
+double gpa = 4.5;
+bool hasScholarship = false;
+
+int currentYear = 2026;
+int age = currentYear - birthYear;
+
+Console.WriteLine("Студенческое удостоверение");
+Console.WriteLine($"Имя: {firstName} {lastName}");
+Console.WriteLine($"Группа: {group}");
+Console.WriteLine($"Возраст: {age} лет");
+Console.WriteLine($"Средний балл: {gpa}");
+Console.WriteLine($"Стипендия: {hasScholarship}");
+
+Console.Write("\nВведите ваш любимый предмет: ");
+string subject = Console.ReadLine();
+Console.WriteLine($"Отлично! {firstName} любит {subject}.");
