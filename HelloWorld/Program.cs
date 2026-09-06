a416993bb565c33b522c126628f99eca1fa698bb
Violl0 <maxim99911@bk.ru>
2026-09-06 13:01:47 +0300

    Step 6: first C# program running

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет от ИСП-241");
+Console.WriteLine("Автор: Каторжин Максим");
+Console.WriteLine("Гот: 2026");
